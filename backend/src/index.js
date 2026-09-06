c06c410c7f3758a517da978ff76c7228da746573
Amit Barman <ab2762314@gmail.com>
2026-09-06 18:30:34 +0530

    Fix incorrect usage of path.json to path.join in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ import { clerkMiddleware } from '@clerk/express'
 const app=express();
 const PORT=process.env.PORT;
 const FRONTEND_URL=process.env.FRONTEND_URL;
-const publicDir=path.json(process.cwd(),"public");
+const publicDir = path.join(process.cwd(), "public");
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL, credentials:true}));
 app.use(clerkMiddleware());
@@ -20,8 +20,8 @@ if(fs.existsSync(publicDir)){
     app.use(express.static(publicDir))
 
     app.get("/{*any}",(req,res,next)=>{
-        res.sendFile(path.json(publicDir, "index.html"), (err)=>next(err));
-    });
+          res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    })
 }
 
 app.listen(PORT, ()=> 
